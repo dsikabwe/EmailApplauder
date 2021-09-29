b956b4b086046fa2dfd1fc62082206c783742d0a
David Sikabwe <dsikabwe@gmail.com>
2021-09-29 01:52:46 -0500

    Cleaning up script.js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 // We're checking for mutations of the DOM——specifcally, the
-// element that says "Sending...". This code will break if a dev
-// on the Gmail team so much as sneezes.
+// element that says "Sending...".
 
 window.onload = function () {
 	const vh = document.querySelectorAll('div.vh')
-
-	console.log(vh.length)
 	const targetNode = vh[0] // div with class vh
-	console.log(targetNode)
 
 	const observerOptions = {
 		childList: true,
 		attributes: true,
-		// Omit (or set to false) to observe only changes to the parent node
 		subtree: false
 	}
 
@@ -25,7 +20,6 @@ function callback(mutationList, observer) {
 		if (mutation.type === 'childList') {
 			for (const node of mutation.addedNodes.values()) {
 				if (node.nodeType === Node.TEXT_NODE && node.textContent === "Sending...") {
-					console.log(node)
 					const audio = new Audio(chrome.runtime.getURL("applause.mp3"))
 					audio.play()
 				}
